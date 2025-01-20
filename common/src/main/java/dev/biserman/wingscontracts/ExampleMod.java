5c6c1a5c45f855c06a4bfcf9f6b778ff2b239d3c
winggar <winggar1228@gmail.com>
2025-01-20 00:20:42 -0800

    first commit

---
+++ common/src/main/java/dev/biserman/wingscontracts/ExampleMod.java
@@ -0,0 +1,9 @@
+package dev.biserman.wingscontracts;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "wingscontracts";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
