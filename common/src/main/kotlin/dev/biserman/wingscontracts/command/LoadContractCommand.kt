3d10c5df35c79f8d59c3767bcf33e3b03f787414
winggar <winggar1228@gmail.com>
2025-04-20 14:03:09 -0700

    fix contract load command, resolve #22

--- common/src/main/kotlin/dev/biserman/wingscontracts/command/LoadContractCommand.kt
+++ common/src/main/kotlin/dev/biserman/wingscontracts/command/LoadContractCommand.kt
@@ -7,7 +7,6 @@ import dev.biserman.wingscontracts.WingsContractsMod
 import dev.biserman.wingscontracts.api.AbyssalContract
 import dev.biserman.wingscontracts.api.Contract
 import dev.biserman.wingscontracts.command.ModCommand.giveContract
-import dev.biserman.wingscontracts.data.LoadedContracts
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.commands.arguments.EntityArgument
@@ -33,11 +32,10 @@ object LoadContractCommand {
                         )
                     })
 
-    fun loadContract(jsonString: String): Contract? {
+    fun loadContract(jsonString: String): Contract {
         try {
-            val tag =
-                AbyssalContract.fromJson(JsonParser.parseString(jsonString).asJsonObject) // add support for other types later
-            return LoadedContracts[tag]
+            val tag = AbyssalContract.fromJson(JsonParser.parseString(jsonString).asJsonObject) // add support for other types later
+            return AbyssalContract.load(tag)
         } catch (e: Error) {
             WingsContractsMod.LOGGER.error(e)
             throw e
