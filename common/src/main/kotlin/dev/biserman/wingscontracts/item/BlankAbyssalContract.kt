f0b628ddfe3fc0f79a16951e51e78ca52ab6be9f
winggar <winggar1228@gmail.com>
2025-05-10 02:23:10 -0700

    add bound contract recipe as part of #31. also show in crafting whether or not abyssal contracts can be initialized

--- common/src/main/kotlin/dev/biserman/wingscontracts/item/BlankAbyssalContract.kt
+++ common/src/main/kotlin/dev/biserman/wingscontracts/item/BlankAbyssalContract.kt
@@ -1,15 +1,19 @@
 package dev.biserman.wingscontracts.item
 
+import dev.biserman.wingscontracts.WingsContractsMod
 import dev.biserman.wingscontracts.config.ModConfig
 import dev.biserman.wingscontracts.data.AvailableContractsManager
 import dev.biserman.wingscontracts.registry.ModSoundRegistry
 import dev.biserman.wingscontracts.server.AvailableContractsData
+import net.minecraft.ChatFormatting
+import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
 
 class BlankAbyssalContract(properties: Properties) : Item(properties) {
@@ -34,5 +38,26 @@ class BlankAbyssalContract(properties: Properties) : Item(properties) {
         }
 
         return InteractionResultHolder.pass(itemStack)
+    }
+
+    override fun appendHoverText(
+        itemStack: ItemStack,
+        level: Level?,
+        components: MutableList<Component>,
+        tooltipFlag: TooltipFlag
+    ) {
+        if (ModConfig.SERVER.allowBlankContractInitialization.get()) {
+            components.add(
+                Component
+                    .translatable("item.${WingsContractsMod.MOD_ID}.blank_abyssal_contract.desc.can")
+                    .withStyle(ChatFormatting.GRAY)
+            )
+        } else {
+            components.add(
+                Component
+                    .translatable("item.${WingsContractsMod.MOD_ID}.blank_abyssal_contract.desc.cannot")
+                    .withStyle(ChatFormatting.GRAY)
+            )
+        }
     }
 }
