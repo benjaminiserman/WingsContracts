7dd9dff3b25379e73bcac00968771226cda34af7
winggar <winggar1228@gmail.com>
2025-05-10 01:11:48 -0700

    add more items

--- common/src/main/kotlin/dev/biserman/wingscontracts/registry/ModItemRegistry.kt
+++ common/src/main/kotlin/dev/biserman/wingscontracts/registry/ModItemRegistry.kt
@@ -8,16 +8,29 @@ import net.minecraft.core.registries.Registries
 import net.minecraft.world.item.Item
 
 object ModItemRegistry {
-    val ITEMS: DeferredRegister<Item?> = DeferredRegister.create(
+    val ITEMS: DeferredRegister<Item> = DeferredRegister.create(
         WingsContractsMod.MOD_ID,
         Registries.ITEM
     )
 
-    val ABYSSAL_CONTRACT: RegistrySupplier<Item?> = ITEMS.register(
+    val BLANK_ABYSSAL_CONTRACT: RegistrySupplier<Item> = ITEMS.register(
+        "blank_abyssal_contract"
+    ) { ContractItem(Item.Properties().stacksTo(1)) }
+    val ABYSSAL_CONTRACT: RegistrySupplier<Item> = ITEMS.register(
         "abyssal_contract"
     ) { ContractItem(Item.Properties().stacksTo(1)) }
+    val BLANK_BOUND_CONTRACT: RegistrySupplier<Item> = ITEMS.register(
+        "blank_bound_contract"
+    ) { ContractItem(Item.Properties().stacksTo(1)) }
+    val BOUND_CONTRACT: RegistrySupplier<Item> = ITEMS.register(
+        "bound_contract"
+    ) { ContractItem(Item.Properties().stacksTo(1)) }
 
-    val QUESTION_MARK: RegistrySupplier<Item?> = ITEMS.register(
+
+    val ABYSSAL_COIN: RegistrySupplier<Item> = ITEMS.register(
+        "abyssal_coin"
+    ) { Item(Item.Properties().stacksTo(64)) }
+    val QUESTION_MARK: RegistrySupplier<Item> = ITEMS.register(
         "question_mark"
     ) { Item(Item.Properties().stacksTo(16)) }
 
